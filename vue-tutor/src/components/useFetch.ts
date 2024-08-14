b73400bddae88a3e12aa182304c6c1b402c89ac5
Sam Wade <ahmadlevin.34@gmail.com>
2024-08-14 05:04:48 +0300

    update_useFetch

--- vue-tutor/src/components/useFetch.ts
+++ vue-tutor/src/components/useFetch.ts
@@ -1,42 +1,23 @@
 import { ref, watchEffect, toValue } from 'vue'
 
-export function useFetch(url: any) {
+export function useFetch(url:any) {
   const data = ref(null)
   const error = ref(null)
 
-  watchEffect(async () => {
+  const fetchData = () => {
     // reset state before fetching..
     data.value = null
     error.value = null
 
-    // resolve the url value synchronously so it's tracked as a
-    // dependency by watchEffect()
-    const urlValue = toValue(url)
+    fetch(toValue(url))
+      .then((res) => res.json())
+      .then((json) => (data.value = json))
+      .catch((err) => (error.value = err))
+  }
 
-    try {
-      // artificial delay / random error
-      await timeout()
-      // unref() will return the ref value if it's a ref
-      // otherwise the value will be returned as-is
-      const res = await fetch(urlValue)
-      data.value = await res.json()
-    } catch (e: any) {
-      error.value = e
-    }
+  watchEffect(() => {
+    fetchData()
   })
 
   return { data, error }
-}
-
-// artificial delay
-function timeout() {
-  return new Promise<void>((resolve, reject) => {
-    setTimeout(() => {
-      if (Math.random() > 0.3) {
-        resolve()
-      } else {
-        reject(new Error('Random Error'))
-      }
-    }, 300)
-  })
 }
